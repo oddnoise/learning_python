cf0fd4f2b185ad02982148714b6209fef131c4a6
peter <palewis1222@gmail.com>
2018-04-19 00:13:06 -0400

    Remove indented_variable from app.py
    1 insertion and 2 deletions

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 apples = ["red", "green", "green", "red", "brown", "mauve"]
 
 for apple in apples:
-    indented_variable= "dumb"
     print(apple)
 
 print(apple)
-print(indented_variable)    
+   
